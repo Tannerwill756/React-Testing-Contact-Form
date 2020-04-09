c723ff0d6792a31333406274e7b5121f9e88e7f0
Tanner Williams <tannerwill756@gmail.com>
2020-04-09 19:00:05 -0400

    input testing working, debugging submit testing

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -18,7 +18,8 @@ const ContactForm = () => {
           <input
             name="firstName"
             placeholder="bill"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true, maxLength: 30 })}
+            id="firstName"
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -31,6 +32,7 @@ const ContactForm = () => {
             name="lastName"
             placeholder="luo"
             ref={register({ required: true })}
+            id="lastName"
           />
           {errors.lastName && (
             <p>Looks like there was an error: {errors.lastName.type}</p>
@@ -41,21 +43,21 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name="email" ref={register({ required: true })} id="email"/>
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea name="message" ref={register({ required: false })} id="message"/>
         </div>
         {data && (
-          <pre style={{ textAlign: "left", color: "white" }}>
+          <pre data-testid="preTag" style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input type="submit" data-testid="submit"/>
       </form>
     </div>
   );
--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, getByTestId } from '@testing-library/react';
 import ContactForm from './ContactForm';
 
 // test('fake test',() => {});
@@ -18,4 +18,61 @@ test('Current labels are appearing',() => {
 
     const labelMessage = getByText(/message/i);
     expect (labelMessage).toBeVisible();
+})
+
+test('field allows you to input text', () => {
+    //ARRANGE
+    const { getByLabelText } = render(<ContactForm />);
+
+    const firstNameInput = getByLabelText(/first Name/i);
+    const lastNameInput = getByLabelText(/last Name/i);
+    const emailInput = getByLabelText(/email/i);
+    const messageInput = getByLabelText(/message/i);
+    
+    //ACT
+    fireEvent.change(firstNameInput, { target: { value: "Tanner" } });
+    fireEvent.change(lastNameInput, { target: { value: "Williams" } });
+    fireEvent.change(emailInput, { target: { value: "lajsdfij@gmail.com" } });
+    fireEvent.change(messageInput, { target: { value: "ajsdlkf alksjdflkja" } });
+    //ASSERT
+    expect(firstNameInput.value).toBe("Tanner")
+    expect(firstNameInput).toBeVisible();
+
+    expect(lastNameInput.value).toBe("Williams")
+    expect(lastNameInput).toBeVisible();
+
+    expect(emailInput.value).toBe("lajsdfij@gmail.com")
+    expect(emailInput).toBeVisible();
+
+    expect(messageInput.value).toBe("ajsdlkf alksjdflkja")
+    expect(messageInput).toBeVisible();
+})
+
+test('form can be submitted', () => {
+    //ARRANGE
+    const { getByLabelText, getByTestId } = render(<ContactForm />);
+
+    const firstNameInput = getByLabelText(/first Name/i);
+    const lastNameInput = getByLabelText(/last Name/i);
+    const emailInput = getByLabelText(/email/i);
+    const messageInput = getByLabelText(/message/i);
+    //ACT
+    fireEvent.change(firstNameInput, { target: { value: "Tanner" } });
+    fireEvent.change(lastNameInput, { target: { value: "Williams" } });
+    fireEvent.change(emailInput, { target: { value: "lajsdfij@gmail.com" } });
+    fireEvent.change(messageInput, { target: { value: "ajsdlkf alksjdflkja" } });
+
+
+    fireEvent.click(getByTestId(/submit/i));
+    //ASSERT
+    expect(firstNameInput).toBeVisible();
+    expect(lastNameInput).toBeVisible();
+    expect(emailInput).toBeVisible();
+    expect(messageInput).toBeVisible();
+
+
+    
+    //expects submitted info to be visible
+    const submittedInfo = getByTestId('preTag');
+    expect(submittedInfo).toBeInTheDocument();
 })
